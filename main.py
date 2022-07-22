17e79912f24db23dd3589b6a9e17e941f46ddc8d
Mayank Sinha <mayanksinha@gmail.com>
2022-07-22 11:48:36 +0530

    Added requirments.txt, .env, .gitignore and completed the code with twilio

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 import requests
-import datetime as dt
+from twilio.rest import Client
+from dotenv import load_dotenv
+import os
 
 # One call API 3.0 which is the current verion when I am programming with this It need a paid subscription.
 # So I Use 2.5 version one call API
 
-url = 'https://api.openweathermap.org/data/2.5/onecall'
-api_key = '75e527f8cba8b370f9540b36449b84ad'
+load_dotenv()
+
+weather_url = os.getenv('WEATHER_URL')
+api_key = os.getenv('API_KEY')
 latitude = 22.718670
 longitude = 75.855710
 
-parameters = {
+account_sid = os.getenv('ACCOUNT_SID')
+auth_token = os.getenv('AUTH_TOKEN')
+phone_number = os.getenv('PHONE_NUMBER')
+client_number = os.getenv('CLIENT_NUMBER')
+client = Client(account_sid, auth_token)
+
+weather_parameters = {
     "lat": latitude,
     "lon": longitude,
-    "appid": api_key
+    "appid": api_key,
+    "exclude": "current,minutely,daily",
 }
 
-response = requests.get(url=url, params=parameters)
+response = requests.get(url=weather_url, params=weather_parameters)
 response.raise_for_status()
-data = response.json()
-if __name__ == '__main__':
-    print(data)
+weather_data = response.json()
+hourly_data = weather_data["hourly"][:19]
+
+for time in hourly_data:
+    weather_id = time["weather"][0]["id"]
+
+    if 531 >= weather_id <= 500:
+        message = client.messages \
+            .create(
+                body="Its going to rain dont forget to bring umbrella ☔",
+                from_=phone_number,
+                to=client_number
+        )
+        break
